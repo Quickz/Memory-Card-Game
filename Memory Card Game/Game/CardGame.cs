0cf977ffd8a53172e086a69de00485dddbc0ae5d
Quickz <alexserg@inbox.lv>
2019-11-18 03:25:54 +0200

    refactored some code - moved random hex number generation method to a separate class

--- Memory Card Game/Game/CardGame.cs
+++ Memory Card Game/Game/CardGame.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Memory_Card_Game.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +9,6 @@ namespace Memory_Card_Game.Game
     public class CardGame
     {
         public Card[,] cards;
-        private Random random = new Random();
 
         public CardGame(int cardCountX, int cardCountY)
         {
@@ -18,26 +18,9 @@ namespace Memory_Card_Game.Game
             {
                 for (int j = 0; j < cards.GetLength(1); j++)
                 {
-                    cards[i, j] = new Card("#" + RandomHexNumber(6));
+                    cards[i, j] = new Card("#" + StringUtility.RandomHexNumber(6));
                 }
             }
-        }
-
-        public string RandomHexNumber(int digits)
-        {
-            byte[] buffer = new byte[digits / 2];
-            random.NextBytes(buffer);
-            
-            string result = string
-                .Concat(buffer.Select(x => x.ToString("X2"))
-                .ToArray());
-
-            if (digits % 2 == 0)
-            {
-                return result;
-            }
-
-            return result + random.Next(16).ToString("X");
         }
     }
 }
---
+++ Memory Card Game/Utilities/StringUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Memory_Card_Game.Utilities
+{
+    public static class StringUtility
+    {
+        private static Random random = new Random();
+
+        public static string RandomHexNumber(int digits)
+        {
+            byte[] buffer = new byte[digits / 2];
+            random.NextBytes(buffer);
+
+            string result = string
+                .Concat(buffer.Select(x => x.ToString("X2"))
+                .ToArray());
+
+            if (digits % 2 == 0)
+            {
+                return result;
+            }
+
+            return result + random.Next(16).ToString("X");
+        }
+    }
+}
