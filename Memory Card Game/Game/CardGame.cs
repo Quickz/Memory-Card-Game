39f97a60817e534f25ba6ad0b061309fced416eb
Quickz <alexserg@inbox.lv>
2019-11-18 02:23:38 +0200

    made the generated buttons use the card instance color and made the game assign random color value to each card

--- Memory Card Game/Game/CardGame.cs
+++ Memory Card Game/Game/CardGame.cs
@@ -8,6 +8,7 @@ namespace Memory_Card_Game.Game
     public class CardGame
     {
         public Card[,] cards;
+        private Random random = new Random();
 
         public CardGame(int cardCountX, int cardCountY)
         {
@@ -17,9 +18,26 @@ namespace Memory_Card_Game.Game
             {
                 for (int j = 0; j < cards.GetLength(1); j++)
                 {
-                    cards[i, j] = new Card("#DB4437");
+                    cards[i, j] = new Card("#" + RandomHexNumber(6));
                 }
             }
+        }
+
+        public string RandomHexNumber(int digits)
+        {
+            byte[] buffer = new byte[digits / 2];
+            random.NextBytes(buffer);
+            
+            string result = string
+                .Concat(buffer.Select(x => x.ToString("X2"))
+                .ToArray());
+
+            if (digits % 2 == 0)
+            {
+                return result;
+            }
+
+            return result + random.Next(16).ToString("X");
         }
     }
 }
