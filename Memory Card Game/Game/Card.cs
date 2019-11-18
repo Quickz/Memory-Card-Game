61934d5011bfce5c8fcdb4da2e50962a0d27e174
Quickz <alexserg@inbox.lv>
2019-11-18 02:13:08 +0200

    added 2 new core classes for the game, made the app generate a grid of buttons that'll be used as cards later

---
+++ Memory Card Game/Game/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Memory_Card_Game.Game
+{
+    public class Card
+    {
+        public string Color { get; private set; }
+
+        public bool Revealed { get; set; }
+
+        public Card(string color)
+        {
+            Color = color;
+        }
+    }
+}
---
+++ Memory Card Game/Game/CardGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Memory_Card_Game.Game
+{
+    public class CardGame
+    {
+        public Card[,] cards;
+
+        public CardGame(int cardCountX, int cardCountY)
+        {
+            cards = new Card[cardCountX, cardCountY];
+
+            for (int i = 0; i < cards.GetLength(0); i++)
+            {
+                for (int j = 0; j < cards.GetLength(1); j++)
+                {
+                    cards[i, j] = new Card("#DB4437");
+                }
+            }
+        }
+    }
+}
